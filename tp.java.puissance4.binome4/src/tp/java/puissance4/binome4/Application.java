0dbcc5356d4298091515d7752be24eb00e7089df
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-15 18:24:17 +0100

    Add TODO (Application.java)

--- tp.java.puissance4.binome4/src/tp/java/puissance4/binome4/Application.java
+++ tp.java.puissance4.binome4/src/tp/java/puissance4/binome4/Application.java
@@ -3,36 +3,30 @@ package tp.java.puissance4.binome4;
 import java.util.Scanner;
 
 /**
- * La Classe principale
+ * Classe principale.
+ *
+ * @author TODO
+ * @version TODO
  */
-
 public class Application {
+    /** TODO Pourquoi un attribut ? */
 	protected static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
 		// Saisie du joueur 1 --> couleur Jaune
-
-		System.out.println("Entrez le nom du Joueur n°1 (signe O) : ");
+		System.out.println("Entrez le nom du Joueur nÂ°1 (signe O) : ");
 		String nom1 = scanner.nextLine();
-		System.out.println();
-		System.out.println("Le joueur 1 à bien été enregistré");
-		System.out.println();
+		System.out.println("\nLe joueur 1 a bien Ã©tÃ© enregistrÃ©\n");
 
 		// Saisie du joueur 2 --> couleur Rouge
-
-		System.out.println("Entrez le nom du joueur n°2 (signe X) : ");
+		System.out.println("Entrez le nom du joueur nÂ°2 (signe X) : ");
 		String nom2 = scanner.nextLine();
-		System.out.println();
-		System.out.println("Le joueur 2 à bien été enregistré");
-		System.out.println();
+		System.out.println("\nLe joueur 2 a bien Ã©tÃ© enregistrÃ©\n");
 
 		// Initialisation de la partie
+		System.out.println("\nLa partie est prÃªte Ã  commencer !");
 
-		System.out.println();
-		System.out.println("La partie est prête à commencée !");
-
-		Puissance4 p = new Puissance4(new Joueur(nom1, Jeu.PION_JAUNE),	new Joueur(nom2, Jeu.PION_ROUGE));
-		p.joue();
+		new Puissance4(new Joueur(nom1, Jeu.PION_JAUNE),	new Joueur(nom2, Jeu.PION_ROUGE)).joue();
 	}
 }
